27f52cd6e4600df7a42c6c5aced1a88d16bb2f17
Aditya1026-05 <adityatayal2610@gmail.com>
2026-09-01 01:33:32 +0530

    Time: 70 ms (54.66%), Space: 14.4 MB (61.52%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,26 +1,45 @@
-//Memoization
+// Tabulation
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(int i, int prev, vector<int>& nums){
-        if(i == nums.size()) return 0;
-
-        if(dp[i][prev+1] != -1) return dp[i][prev+1];
-
-        int exclude = solve(i+1, prev, nums);
-        int include = 0;
-
-        if(prev == -1 || nums[i] > nums[prev]){
-            include = 1 + solve(i+1, i, nums);
-        }
-        return dp[i][prev+1] = max(include, exclude);
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        dp.resize(n, vector<int>(n+1,-1));
-        return solve(0, -1, nums);
+        vector<int> lis(n, 1);
+
+        for(int i = 0; i < n ; i++){
+            for(int j = 0; j < i ; j++){
+                if(nums[j] < nums[i]){
+                    lis[i] = max(lis[i], lis[j] + 1);
+                }
+            }
+        }
+        return *max_element(lis.begin(), lis.end());
     }
 };
+
+
+//Memoization
+// class Solution {
+// public:
+//     vector<vector<int>> dp;
+//     int solve(int i, int prev, vector<int>& nums){
+//         if(i == nums.size()) return 0;
+
+//         if(dp[i][prev+1] != -1) return dp[i][prev+1];
+
+//         int exclude = solve(i+1, prev, nums);
+//         int include = 0;
+
+//         if(prev == -1 || nums[i] > nums[prev]){
+//             include = 1 + solve(i+1, i, nums);
+//         }
+//         return dp[i][prev+1] = max(include, exclude);
+//     }
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n = nums.size();
+//         dp.resize(n, vector<int>(n+1,-1));
+//         return solve(0, -1, nums);
+//     }
+// };
 
 
 
