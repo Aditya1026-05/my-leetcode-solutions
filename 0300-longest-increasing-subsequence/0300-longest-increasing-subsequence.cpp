6eefa5a223927e4264a063f494cca4133fe4dda8
Aditya1026-05 <adityatayal2610@gmail.com>
2026-09-01 00:26:40 +0530

    Time: 540 ms (21.81%), Space: 296.4 MB (27.78%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,46 @@
+//Memoization
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int solve(int i, int prev, vector<int>& nums){
+        if(i == nums.size()) return 0;
+
+        if(dp[i][prev+1] != -1) return dp[i][prev+1];
+
+        int exclude = solve(i+1, prev, nums);
+        int include = 0;
+
+        if(prev == -1 || nums[i] > nums[prev]){
+            include = 1 + solve(i+1, i, nums);
+        }
+        return dp[i][prev+1] = max(include, exclude);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        dp.resize(n, vector<int>(n+1,-1));
+        return solve(0, -1, nums);
+    }
+};
+
+
+
+//Brute Force
+
+
+// class Solution {
+// public:
+//     int solve(int i, int prev, vector<int>& nums){
+//         if(i == nums.size()) return 0;
+
+//         int exclude = solve(i+1, prev, nums);
+//         int include = 0;
+
+//         if(prev == -1 || nums[i] > nums[prev]){
+//             include = 1 + solve(i+1, i, nums);
+//         }
+//         return max(include, exclude);
+//     }
+//     int lengthOfLIS(vector<int>& nums) {
+//         return solve(0, -1, nums);
+//     }
+// };
