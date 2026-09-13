04344844cd4f13c92784ca32c76daee84b6e8da4
Aditya1026-05 <adityatayal2610@gmail.com>
2026-09-13 23:11:31 +0530

    Time: 0 ms (100%), Space: 13.4 MB (29.32%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        vector<int> ans;
-        int high = *max_element(arr.begin(), arr.end())+k;
+        int low = 0;
+        int high = arr.size() - 1;
 
-        unordered_map<int, int> mp;
+        while(low <= high){
+            int mid = low + (high - low)/2;
 
-        for(int i = 0; i < arr.size(); i++){
-            mp[arr[i]]++;
-        }
+            int missing = arr[mid] - (mid+1);
 
-        for(int i = 1; i <= high; i++){
-            if(mp.find(i) == mp.end()){
-                ans.push_back(i);
+            if(missing >= k){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
             }
         }
-        return ans[k-1];
+        return low+k;
     }
 };
+
+//Brute Force
+
+// class Solution {
+// public:
+//     int findKthPositive(vector<int>& arr, int k) {
+//         vector<int> ans;
+//         int high = *max_element(arr.begin(), arr.end())+k;
+
+//         unordered_map<int, int> mp;
+
+//         for(int i = 0; i < arr.size(); i++){
+//             mp[arr[i]]++;
+//         }
+
+//         for(int i = 1; i <= high; i++){
+//             if(mp.find(i) == mp.end()){
+//                 ans.push_back(i);
+//             }
+//         }
+//         return ans[k-1];
+//     }
+// };
